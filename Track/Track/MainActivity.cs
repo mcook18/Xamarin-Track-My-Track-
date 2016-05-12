ce3918118e7d30b64cead5b260d995631f9d5bbd
unknown <mcook18@LAB-SCI-304-15.whitworth.edu>
2016-05-11 18:28:49 -0700

    SQL and edit-text boxes

--- Track/Track/MainActivity.cs
+++ Track/Track/MainActivity.cs
@@ -16,7 +16,11 @@ namespace Track
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            var editTextEM = FindViewById<EditText>(Resource.Id.ETemail);
+            var editTextPW = FindViewById<EditText>(Resource.Id.ETpassword);
+            var textView = FindViewById<TextView>(Resource.Id.textView);
+            var textView2 = FindViewById<TextView>(Resource.Id.textView);
+            var Changed = false;
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
@@ -31,14 +35,27 @@ namespace Track
             };
 
             Button login = FindViewById<Button>(Resource.Id.LoginButton);
-            login.Click += delegate {
+            login.Enabled = false;
+             login.Click += delegate {
+                if (Changed==true)
                 // SetContentView(Resource.Layout.Menu);//takes to menu page
                 StartActivity(typeof(Track.Resources.Menu));
-                Android.Widget.Toast.MakeText(this, "Logging in!", Android.Widget.ToastLength.Short).Show();
+                    Android.Widget.Toast.MakeText(this, "Logging in!", Android.Widget.ToastLength.Short).Show();
+            
+            };
+            // I am seeing if we can get the edit text boxes set up and running before I write the PHP
+           
+            editTextEM.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+              {
+                  Changed = true;
+                  login.Enabled = true;
+                  textView.Text = e.Text.ToString();
+              };
+            editTextPW.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            {
+                textView2.Text = e.Text.ToString();
             };
 
-           
-            
         }
     }
 }
